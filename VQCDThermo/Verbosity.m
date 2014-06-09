5058f639f12ef2521382cfa4c349bee5fc3cffa9
Timo Alho <timo.s.alho@jyu.fi>
2014-06-09 15:48:23 +0300

    Add a new function for debug-analysis on conditionals.
    Add AnalyzeConditional and AnalyzeConditionalV, which
    print out the truth values of sub-expressions, 
    separated by And, Or, or Xor, for debug purposes.
    A parameter is provided for connecting this to the
    same verbosity system as PrintV.
    
    As a side-effect, some refactoring was done to prevent
    code duplication.

--- VQCDThermo/Verbosity.m
+++ VQCDThermo/Verbosity.m
@@ -13,6 +13,11 @@ BeginPackage["VQCDThermo`Verbosity`"]
 PrintV::usage = "PrintV[output, level] conditionally prints output, depending on level and the current setting of SetVerbosity. If level <= GetVerbosity[], PrintV[output, level] is equivalent to Print[output], otherwise it does nothing.
 
 The first parameter is only evaluated after checking the level, so lengthy Plot[...]'s etc will not take time if the current verbosity level is below the level determined in PrintV."
+EvaluateV::usage = "EvaluateV[level, expr1, expr2] conditionally evaluates expr1 or expr2, depending on level and the current setting of SetVerbosity. If level <= GetVerbosity[], expr1 is evaluated, otherwise expr2."
+
+(*The next two functions may be at a slightly wrong place, or perhaps this package could be renamed DebugTools etc.*)
+AnalyzeConditional::usage = "Subdivides conditional expressions at operators And, Or and Xor, and prints out truth values of the subexpressions. Returns the value of the full expression";
+AnalyzeConditionalV::usage = "Evaluates the given conditional, or applies AnalyzeConditional, depending on verbosity"
 
 SetVerbContext::usage = "Sets the context for the verbosity, to allow varying the verbosity level locally."
 ResetVerbContext::usage = "Resets the verb context to the previous value."
@@ -53,19 +58,47 @@ GetVContextLevel[context_] := vcontextlevels[ToString[context]];
 GetVerbosity[] := Verbosity;
 
 SetAttributes[PrintV, HoldFirst](*This prevents evaluation of the first argument of PrintV. This way calling for example PrintV[Plot[Slowfunction ... ], "Debug"] will not spend time calculating the plot if $Verbosity is below "Debug".*)
+SetAttributes[EvaluateV, HoldRest]
 
-PrintV[out_, level_?Verbtest] := ( 
+(*Evaluates expr1 or expr2 based on verbosity.*)
+EvaluateV[level_?Verbtest, expr1_, expr2_ : Null] := If[(VerbLevels[level] >  Verbosity) && !TrueQ[(VerbLevels[level] <= vcontextlevels[$vcontext])],
+	expr2,
+	expr1,
+	expr1
+];
+
+PrintV[out_, level_?Verbtest] := EvaluateV[level, Print[out], Null];
+
+SetAttributes[AnalyzeConditional, HoldFirst]
+AnalyzeConditional[expr_, topLevel_ : True]:= Module[{
+comps = (Hold/@Unevaluated[expr]),
+result = expr},
+If[topLevel,
+	Print[StringForm["Expression `1`\n\t is `2` ", HoldForm[expr], result]];
+];
+Function[x, AnalyzeConditional[Unevaluated[x], False], HoldFirst] @@@ comps;
+result
+]/;MatchQ[Unevaluated@expr,_And|_Or|_Xor|_Not];
+AnalyzeConditional[expr_, False] := Print[StringForm["Subexpression `1`\n\t is `2`", HoldForm[expr], expr]];
+
+SetAttributes[AnalyzeConditionalV, HoldFirst]
+AnalyzeConditionalV[expr_, level_?Verbtest] := EvaluateV[level, AnalyzeConditional[expr], expr];
+
+(*PrintV[out_, level_?Verbtest] := ( 
 (*Print[StringForm["Current vcontextlevel = `1`", vcontextlevels[Last[vcontext]]]];*)
 (*Print[StringForm["vcontextlevels = `1`, current vcontext = `2`", DownValues[vcontextlevels], $vcontext]];*)
 If[(VerbLevels[level] >  Verbosity) && !TrueQ[(VerbLevels[level] <= vcontextlevels[$vcontext])],
 Return[]];
 (*Print[StringForm["Current evaluation stack `1`",stack]];*)
 Print[out];
-);
+);*)
 
 End[ ]
 
 EndPackage[ ]
+
+
+
 
 
 
